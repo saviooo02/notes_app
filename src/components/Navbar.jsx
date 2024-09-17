ced7f0c9a1db9e650166f6b3589284893e262603
Savio Shaji <savioshaji02@gmail.com>
2024-09-17 23:28:37 +0530

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar(){
+    return(
+        <>
+            <div className="bg-cyan-900">
+                <p>Hello</p>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
