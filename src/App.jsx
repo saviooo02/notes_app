95d6928bf5febc92b71077090da4f834d08bd1df
Savio Shaji <savioshaji02@gmail.com>
2024-09-18 22:05:10 +0530

    mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { useState, useEffect } from "react";
 import Hero from "./components/Hero";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 
 function App() {
- 
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
   return (
-    <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-        <Navbar></Navbar>
-        <Hero></Hero>
+    <div className={`absolute inset-0 -z-10 h-full w-full transition-colors duration-300 ${isDarkMode ? "h-full w-full bg-slate-950 bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" : "absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"}`}>
+      <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Hero isDarkMode={isDarkMode} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,16 +2,14 @@ function Hero() {
     return (
       <>
         <div className="relative min-h-[calc(100vh-56px)] flex justify-center items-center">
-          <p className="text-2xl font-inter md:text-4xl lg:text-5xl text-neutral-500 text-center font-bold absolute top-32 md:px-14 ">
+          <p className="text-2xl font-inter md:text-4xl lg:text-5xl text-neutral-500 text-center font-bold absolute top-32 md:px-14 dark:text-neutral-200 ">
             Turn your ideas into structured notes <br />with ease, all in one place.
           </p>
-            <button type="button" className="text-neutral-500 font-inter hover:bg-neutral-200 font-bold rounded-full text-sm px-5 py-2.5 me-2 lg:mb-40 md:mb-48 sm:mb-60 mb-60 flex">Get Started
+            <button type="button" className="text-neutral-500 font-inter hover:bg-neutral-200 font-bold rounded-md text-sm px-5 py-2.5 me-2 lg:mb-40 md:mb-48 sm:mb-60 mb-60 flex dark:text-neutral-200 dark:hover:bg-neutral-700">Get Started
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 ml-4">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
                 </svg>
             </button>
-           
-
         </div>
       </>
     );
