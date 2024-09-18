498a132dd98ad4d10519a844363bd78a4c2b28c6
Savio Shaji <savioshaji02@gmail.com>
2024-09-18 19:14:31 +0530

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar"
 
 function App() {
  
   return (
-    <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+    <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
         <Navbar></Navbar>
+        <Hero></Hero>
     </div>
   )
 }
